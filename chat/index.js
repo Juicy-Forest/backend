4f6ffd4ce96ae36060a02a84e41213fcb1989f96
kebabzter <gabzterthesecond@gmail.com>
2025-12-14 21:02:13 +0100

    added cookies and jwt verification

--- chat/index.js
+++ chat/index.js
@@ -2,34 +2,44 @@ import express from 'express';
 import { createServer } from 'http'
 import WebSocket, { WebSocketServer } from 'ws';
 import dotenv from 'dotenv'
+import cookie from 'cookie';
+import jwt from 'jsonwebtoken';
 dotenv.config();
 
 const app = express();
 const server = createServer(app);
 const PORT = process.env.PORT || 3033;
+const JWT_SECRET = process.env.JWT_SECRET || 'JWT-SECRET-TOKEN';
 
 const wss = new WebSocketServer({ server });
 
 wss.on('connection', function connection(ws, req) {
+  const cookies = cookie.parse(req.headers.cookie || '');
+  const token = cookies['auth-token'];
+
+  if (!token) {
+    console.log('No authentication cookie')
+    ws.close(1008, 'authentication required')
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    ws.user = decoded;
+  } catch (error) {
+    ws.close(1008, 'Invalid token');
+    return;
+  }
+
   ws.on('error', console.error);
 
   ws.id = Date.now();
-  //ws.verifiedUserId = req.userId;
-
-  const welcomeMessage = JSON.stringify({
-    type: 'id_assignment',
-    clientId: ws.id,
-    message: `Welcome! Your ID is ${ws.id}`
-  });
-
-  ws.send(welcomeMessage);
 
   ws.on('message', function message(load) {
     const data = JSON.parse(load);
-    const text = `user_${data.id}: ${data.message}`;
-    console.log(`message: ${data.message}`)
+    const text = `${data.message}`;
     const response = JSON.stringify({
-      type: 'text',
+      username: ws.user.username,
       message: text
     })
     wss.clients.forEach(function each(client) {
