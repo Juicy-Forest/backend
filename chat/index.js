c4c9aaf4cceaadee999fd673db8c4d081c4167ab
kebabzter <gabzterthesecond@gmail.com>
2025-12-25 20:25:09 +0100

    Extracted database fuction into a utils directory

--- chat/index.js
+++ chat/index.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import { createServer } from 'http';
 import { WebSocketServer } from 'ws';
 import dotenv from 'dotenv';
 import { handleConnection } from './controllers/socketController.js';
-import mongoose from 'mongoose';
+import { initDatabase } from './utils/initDatabase.js';
 
 dotenv.config();
 
@@ -13,15 +12,9 @@ const server = app.listen(PORT, () => {
   console.log(`Chat microservice running on http://localhost:${PORT}`);
 });
 
+initDatabase();
 
 const wss = new WebSocketServer({server});
-
-function initDatabase(){
-    const dbUri = process.env.MONGO_URI || "mongodb://localhost:27017/juicy-forest";
-    return mongoose.connect(dbUri);
-}
-
-initDatabase();
 
 // Delegate connection handling to the controller
 wss.on('connection', async (ws, req) => await handleConnection(wss, ws, req));
